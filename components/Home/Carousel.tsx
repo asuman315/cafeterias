e24ac3c9fc12cae2d92f8f49c9fd7b0ab03391d2
asumanwebdev <asumanwebdev@gmail.com>
2022-06-25 22:29:32 +0300

    added state for handling currentslide

--- components/Home/Carousel.tsx
+++ components/Home/Carousel.tsx
@@ -1,24 +1,24 @@
 import imageOne from '../../images/image-two.jpg';
 import imageTwo from '../../images/image-six.jpg';
 import imageThree from '../../images/image-seven.jpg';
+import { useState, useEffect } from 'react';
+
 //import imageFour from '../../images/image-four.jpg'
 
 console.log(imageOne);
 
 const Carousel = () => {
-  const imageStyles = {
-
-  };
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const images = [ imageOne, imageTwo, imageThree ];
 
   return (
     <section className='border-2 overflow-hidden'>
-      <article className='whitespace-nowrap'>
+      <article className={`whitespace-nowrap translate-x-[${-currentSlide * 100}%]`}>
         {images.map((image, index) => {
           return (
             <div key={index} className='inline-block w-full'>
-              <img src={image.src} style={imageStyles} />
+              <img src={image.src} className='w-screen h-screen' />
             </div>
           );
         })}
