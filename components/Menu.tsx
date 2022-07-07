bf906d2e0cbd9670f0427d02efa25476d1acfd62
asumanwebdev <asumanwebdev@gmail.com>
2022-07-07 23:50:56 +0300

    add categories routes

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,21 +1,21 @@
-
+import { useRouter } from 'next/router';
 
 //This component displays the menu screee
 
 const Menu = ({ categoryData }: any) => {
-  
+   const router = useRouter();
   return (
     <main className='max-w-6xl mx-auto'>
       <div className="grid grid-cols-2 gap-4 my-8 px-4">
         {categoryData.map((category: any) => {
-          const id = category.id;
+          const productId = category.id;
           const name = category.attributes.Name;
           const imageUrl = category.attributes.image.data[0].attributes.url;
           
           return (
             <div
-              key={id}
-              className='shadow-xl lg:cursor-pointer'>
+              key={productId}
+              className='shadow-xl lg:cursor-pointer' onClick={() => router.push(`/subcategory/${productId}`)}>
               <h2 className="pb-2">{name}</h2>
               <img src={imageUrl} alt={`image of ${name}`} className='rounded-md' />
             </div>
---
+++ pages/subcategory/[productId]/index.tsx
@@ -0,0 +1,95 @@
+import { GetStaticProps , GetStaticPaths, /*GetServerSideProps*/ } from 'next';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+export default function SubcategoryPage({ subcategoryData }: any) {
+
+  const name = subcategoryData.attributes.Name;
+  
+  return (
+    <div>
+      <h1>Subcategory Page {name} </h1>
+    </div>
+  );
+}
+
+export  const getStaticProps: GetStaticProps = async (context: any) => {
+  //The id of the product displayed on the product/details page.
+  const productId = context.params.productId
+
+  const client = new ApolloClient({
+    uri: 'https://api-strapi-one.herokuapp.com/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  //(id: ${productId})
+  const { data } = await client.query({
+    query: gql`
+      query {
+        mealcategory(id: ${productId}) {
+          data {
+            attributes {
+              Name
+              mealsubcategories {
+                data {
+                  id
+                  attributes {
+                    name
+                    image {
+                      data {
+                        attributes {
+                          url
+                        }
+                      }
+                    }
+                  }    
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  const subcategoryData = data.mealcategory.data;
+
+  return {
+    props: {
+      subcategoryData,
+    },
+  };
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+
+  const client = new ApolloClient({
+    uri: 'https://api-strapi-one.herokuapp.com/graphql',
+    cache: new InMemoryCache(),
+  });
+
+   const { data } = await client.query({
+     query: gql`
+      query {
+        mealcategories {
+          data {
+            id
+          }
+        }
+      }
+    `,
+   });
+
+   const productsData = data.mealcategories.data;   
+
+  const paths = productsData.map((product: any) => {
+    return { params: { productId: product.id } };
+  });
+
+  console.log('paths', paths);
+  
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
