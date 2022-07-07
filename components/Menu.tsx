2a0d3c4a7c0df6de90ff270694123170cafd742d
asumanwebdev <asumanwebdev@gmail.com>
2022-07-07 19:39:45 +0300

    fetch data for menu items

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,9 +2,31 @@
 
 //This component displays the menu screee
 
-const Menu = () => {
+const Menu = ({ categoryData }: any) => {
+  console.log(categoryData);
+  
   return (
-    <h1>Menu</h1>
+    <section>
+      <div>
+        { categoryData.map((category: any) => { 
+          const id = category.id;
+          const name = category.attributes.name;
+          const imageUrl = category.attributes.image.data[0].attributes.url;
+          return (
+            <div
+              key={id}
+              className='flex flex-col items-center bg-white shadow-xl relative rounded-md lg:cursor-pointer'>
+              <img
+                src={imageUrl}
+                alt={`image of ${name}`}
+                className='rounded-t-md cursor-zoom-in'
+              />
+            </div>
+          );
+        }
+        )}
+      </div>
+    </section>
   )
 }
 
--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,7 +1,48 @@
 import Menu from "../components/Menu"
+import { GetStaticProps /*, GetStaticPaths, GetServerSideProps*/ } from 'next';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-export default function MenuComponent() {
+export default function MenuComponent({ categoryData }: any) {
   return (
-     <Menu />
+     <Menu categoryData={categoryData} />
   )
 }
+
+ 
+export const getStaticProps: GetStaticProps = async () => {
+  const client = new ApolloClient({
+    uri: 'https://api-strapi-one.herokuapp.com/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query {
+        mealcategories {
+          data {
+            id
+            attributes {
+              Name
+              image {
+                data {
+                  attributes {
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  const categoryData = data.mealcategories.data;
+
+  return {
+    props: {
+      categoryData
+    },
+  };
+};
+
