4c4bf0adc649331fd577e846d8d23dbb36fbb5a5
asumanwebdev <asumanwebdev@gmail.com>
2022-07-11 00:53:53 +0300

    finally refactored the options component

--- components/ProductDetails.tsx
+++ components/ProductDetails.tsx
@@ -2,7 +2,6 @@ import { MdKeyboardArrowUp } from 'react-icons/md';
 import { useState } from 'react';
 
 const ProductDetails = ({ mealData }: any) => {
-
   const name = mealData.attributes.name;
   const productImage = mealData.attributes.image.data.attributes.url;
   const choiceOfComponents = mealData.attributes.choiceOfComponents;
@@ -26,161 +25,158 @@ const ProductDetails = ({ mealData }: any) => {
 
 //ProductInfo component
 const ProductInfo = ({ mealData }: any) => {
-   const price = mealData.attributes.price;
-   const components = mealData.attributes.components;
-   const name = mealData.attributes.name;
+  const price = mealData.attributes.price;
+  const components = mealData.attributes.components;
+  const name = mealData.attributes.name;
 
-   return (
+  return (
     <>
       <h1 className='text-2xl text-primary-1'>{name}</h1>
-        <p className='font-medium leading-8 tracking-wide'>{components}</p>
-        <p className='font-bold text-primary-1 leading-8 tracking-wider text-3xl py-3'>
-          ${price}
-        </p>
+      <p className='font-medium leading-8 tracking-wide'>{components}</p>
+      <p className='font-bold text-primary-1 leading-8 tracking-wider text-3xl py-3'>
+        ${price}
+      </p>
     </>
-   )
-}
+  );
+};
 
 // ChoicesOfComponents component
 const ChoicesOfComponents = ({ choiceOfComponents }: any) => {
-  const [openedChoice, setOpenedChoice] = useState(null);
-  //const [isChoiceSelected, setIsChoiceSelected] = useState(false);
-  // const [selectedChoice, setSelectedChoice] = useState(null);
-
-  const handleClick = (openedId: any) => {
-    choiceOfComponents.map((option: any, index: any) => {
-      option ? console.log('option component', option.component) : console.log('No options provided!');   
-      
-      if (openedId === index) {
-        setOpenedChoice(openedId);
-      }
-      if (openedChoice === index) {
-        setOpenedChoice(null);
-      }
-    });
-  };
-
-  // type SelectedChoice = {
-  //   option: string,
-  //   optionId: number,
-  // };
-
-  // const selectedChoices: SelectedChoice[] = []
-
-  // const handleSelectedChoice = (option: string, optionId: number) => {
-  //     const choice: SelectedChoice = { option, optionId };
-  //     //check if choice is already selected/already in the array
-  //     const existingChoice = selectedChoices.find((choice: SelectedChoice) => choice.optionId === optionId);
-
-  //     if (existingChoice) {
-  //       //replace existing choice
-  //       existingChoice.option = option;
-  //     } else {
-  //       //add new choice
-  //       selectedChoices.push(choice);
-  //     }
-  // }
-
-  // const selectedChoice = (optionId: number) => {
-  //     const matchingChoice = selectedChoices.find((choice: SelectedChoice) => choice.optionId === optionId);
-  //     return matchingChoice?.option;
-  // }
-
- //console.log('selectedChoices', selectedChoices); 
-
   return (
     <div>
       {choiceOfComponents.map((item: any, index: any) => {
         const { component, options } = item;
+        const [isChoiceOpened, setIsChoiceOpened] = useState(false);
+        const [choice, setChoice] = useState(null);
+        const [isChoiceSelected, setIsChoiceSelected] = useState(false);
         //turn options into a list
         const optionsList = options.split(',');
-        const openedId: number = index;
+
+        const handleClick = () => {
+          setIsChoiceOpened(!isChoiceOpened);
+        };
 
         return (
-          <div className='mt-4' key={index}>
-            <h3 className='uppercase md:text-xl mb-1'>choice of {component}</h3>
-            <div className='border-2 rounded-md'>
-              <div
-                className='flex justify-between items-center cursor-pointer px-2'
-                onClick={() => handleClick(openedId)}>
-                <p className='font-medium w-full capitalize tracking-wide text-lg'> 
-                  select the option
-                </p>
-                <MdKeyboardArrowUp
-                  className={`w-10 h-10 ease-in duration-300 ${
-                    openedChoice === index ? null : 'rotate-[180deg]'
-                  }`}
-                />
-              </div>
-              <ul
-                className={`${
-                  openedChoice === index ? 'h-auto' : 'h-0'
-                } overflow-hidden`}>
-                {optionsList.map((option: any, index: any) => {
-                  return (
-                    <li
-                      key={index}
-                      className='capitalize px-2 font-medium text-md cursor-pointer hover:bg-primary-1'>
-                      {option}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
+          <SingleChoiceOfComponent
+            component={component}
+            optionsList={optionsList}
+            handleClick={handleClick}
+            isChoiceOpened={isChoiceOpened}
+            choice={choice}
+            isChoiceSelected={isChoiceSelected}
+            setChoice={setChoice}
+            setIsChoiceSelected={setIsChoiceSelected}
+            setIsChoiceOpened={setIsChoiceOpened}
+            key={index}
+          />
         );
       })}
     </div>
   );
-}
+};
+
+// SingleChoiceOfComponent component
+const SingleChoiceOfComponent = ({
+  component,
+  optionsList,
+  handleClick,
+  isChoiceOpened,
+  isChoiceSelected,
+  choice,
+  setChoice,
+  setIsChoiceSelected,
+  setIsChoiceOpened,
+}: any) => {
+
+  const handleSelectedChoice = (option: any) => {
+    setChoice(option);
+    setIsChoiceSelected(true);
+    setIsChoiceOpened(!isChoiceOpened);
+  };
+
+  return (
+    <div className='mt-4'>
+      <h3 className='uppercase md:text-xl mb-1'>choice of {component}</h3>
+      <div className='border-2 rounded-md'>
+        <div
+          className='flex justify-between items-center cursor-pointer px-2'
+          onClick={handleClick}>
+          <p className='font-medium w-full capitalize tracking-wide text-lg'>
+            {isChoiceSelected ? choice : 'select an option'}
+          </p>
+          <MdKeyboardArrowUp
+            className={`w-10 h-10 ease-in duration-300 ${
+              isChoiceOpened ? null : 'rotate-[180deg]'
+            }`}
+          />
+        </div>
+        <ul className={`${isChoiceOpened ? 'h-auto' : 'h-0'} overflow-hidden`}>
+          {optionsList.map((option: any, index: any) => {
+            return (
+              <li
+                key={index}
+                className='capitalize px-2 font-medium text-md cursor-pointer hover:bg-primary-1'
+                onClick={() => handleSelectedChoice(option)}>
+                {option}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 // Accompaniment component
 const Accompaniment = ({ accompaniment }: any) => {
-  const [showOptions, setShowOptions] = useState(false);
+  const [openedChoice, setOpenedOptions] = useState(false);
   const accompanimentList = accompaniment.split(',');
   const [accompanimentState, setAccompanimentState] = useState(null);
   const [isAccompanimentSelected, setIsAccompanimentSelected] = useState(false);
-  
+
   const handleClick = () => {
-    setShowOptions(!showOptions);
+    setOpenedOptions(!openedChoice);
   };
 
   const handleSelectedAccompaniment = (option: any) => {
     setAccompanimentState(option);
     setIsAccompanimentSelected(true);
-    setShowOptions(!showOptions);
-  }
+    setOpenedOptions(!openedChoice);
+  };
 
- return (
-   <div className='mt-8'>
-     <h3 className='uppercase md:text-xl mb-1'>choose an accompaniment</h3>
-     <div className='border-2 rounded-md cursor-pointer'>
-       <div className='flex justify-between items-center' onClick={handleClick}>
-         <p className='font-medium capitalize tracking-wide text-lg px-2'>
-           { isAccompanimentSelected ? accompanimentState : 'select an accompaniment' }
-         </p>
-         <MdKeyboardArrowUp
-           className={`w-10 h-10 ease-in duration-300 ${
-             showOptions ? null : 'rotate-[180deg]'
-           }`}
-         />
-       </div>
-       <ul className={`${showOptions ? 'h-auto' : 'h-0'} overflow-hidden`}>
-         {accompanimentList.map((option: any, index: any) => {
-           return (
-             <li
-               key={index}
-               className='capitalize px-2 font-medium text-md cursor-pointer hover:bg-primary-1'
-               onClick={() => handleSelectedAccompaniment(option)}>
-               {option}
-             </li>
-           );
-         })}
-       </ul>
-     </div>
-   </div>
- );
-
-  }
+  return (
+    <div className='mt-8'>
+      <h3 className='uppercase md:text-xl mb-1'>choose an accompaniment</h3>
+      <div className='border-2 rounded-md cursor-pointer'>
+        <div
+          className='flex justify-between items-center'
+          onClick={handleClick}>
+          <p className='font-medium capitalize tracking-wide text-lg px-2'>
+            {isAccompanimentSelected
+              ? accompanimentState
+              : 'select an accompaniment'}
+          </p>
+          <MdKeyboardArrowUp
+            className={`w-10 h-10 ease-in duration-300 ${
+              openedChoice ? null : 'rotate-[180deg]'
+            }`}
+          />
+        </div>
+        <ul className={`${openedChoice ? 'h-auto' : 'h-0'} overflow-hidden`}>
+          {accompanimentList.map((option: any, index: any) => {
+            return (
+              <li
+                key={index}
+                className='capitalize px-2 font-medium text-md cursor-pointer hover:bg-primary-1'
+                onClick={() => handleSelectedAccompaniment(option)}>
+                {option}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
