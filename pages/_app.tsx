05073750eb763eecfd8538fc44f735b7444cc78d
asumanwebdev <asumanwebdev@gmail.com>
2022-07-14 12:57:39 +0300

    configure redux store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import { Provider } from 'react-redux';
+import store from '../store';
 
 function MyApp({ Component, pageProps } : AppProps) {
   return (
+    <Provider store={store}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </Provider>
   )
 }
 
