fc018e6afd90e7023e4302400ffac225b77d7ec7
asumanwebdev <asumanwebdev@gmail.com>
2022-07-20 15:29:26 +0300

    created cart component

---
+++ pages/user/cart/index.tsx
@@ -0,0 +1,13 @@
+import Cart from "../../../components/Cart";
+
+
+export default function CartPage() {
+  return (
+    <main>
+      <section>
+        <Cart />
+      </section>
+    </main>
+  );
+}
+
