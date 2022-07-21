4616004c84c0b454067e1c70284c73217bcaedad
asumanwebdev <asumanwebdev@gmail.com>
2022-07-21 19:47:30 +0300

    add increament and decrement functionality

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -8,6 +8,7 @@ const Cart = () => {
    useEffect(() => {
     const myCartItems = localStorage.getItem('userCart') ?  JSON.parse(localStorage.getItem('userCart')!) : [];
     setCartItems(myCartItems);
+    //eslint-disable-next-line
    }, []);
 
    console.log('typeof cartItems', typeof cartItems);
@@ -19,37 +20,23 @@ const Cart = () => {
   )
 }
 
-
-const WithCartItems = ({ cartItems }: any) => {
- const [quantity, setQuantity] = useState(1);
+const WithCartItems = ({cartItems}: any) => {
     console.log(cartItems);
+
+    const handleIncreament = (id: number) => {
+
+    }
+
+    const handleDecreament = (id: number) => {
+
+    }
+
   return (
    <div>
       { cartItems.map((item: any, index: any) => {
-       const { name, price, productImage } = item;
         return (
           <div key={index} className='flex py-4 border-b-[1px]'>
-            <img
-              src={productImage}
-              alt={`Image of ${productImage}`}
-              className='w-36 sm:w-44 sm:h-auto h-32 rounded'
-            />
-            <div className='pl-4 w-full'>
-              <h3>{name}</h3>
-              <h4 className='py-2'>Details</h4>
-              <div className='flex justify-between py-4'>
-                <div className='flex items-center'>
-                  <div className='bg-primary-4 w-7 h-7 flex items-center justify-center cursor-pointer'>
-                    <HiPlus />
-                  </div>
-                  <p className='px-2 font-bold text mx-1'>{quantity}</p>
-                  <div className='bg-primary-4 w-7 h-7 flex items-center justify-center cursor-pointer'>
-                    <HiMinus />
-                  </div>
-                </div>
-                <h3 className="">${price}</h3>
-              </div>
-            </div>
+            <CartItem item={item} /> 
           </div>
         );
       }) }
@@ -67,12 +54,39 @@ const WithoutCartItems = () => {
 }
 
 
-const cartItem = () => {
+const CartItem = ({ item }: any) => {
+    const [quantity, setQuantity] = useState(1);
+    const { name, price, productImage } = item;
   return (
     <div>
-
+      {/*eslint-disable-next-line*/}
+      <img
+        src={productImage}
+        alt={`Image of ${productImage}`}
+        className='w-36 sm:w-44 sm:h-auto h-32 rounded'
+      />
+      <div className='pl-4 w-full'>
+        <h3>{name}</h3>
+        <h4 className='py-2'>Details</h4>
+        <div className='flex justify-between py-4'>
+          <div className='flex items-center'>
+            <div
+              className='bg-primary-4 w-7 h-7 flex items-center justify-center cursor-pointer'
+              onClick={() => setQuantity(quantity + 1)}>
+              <HiPlus />
+            </div>
+            <p className='px-2 font-bold text mx-1'>{quantity}</p>
+            <div
+              className='bg-primary-4 w-7 h-7 flex items-center justify-center cursor-pointer'
+              onClick={() => setQuantity(quantity - 1)}>
+              <HiMinus />
+            </div>
+          </div>
+          <h3 className=''>${price}</h3>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 
