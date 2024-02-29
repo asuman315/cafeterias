ea93a4cdd1771c1b04753867a30084320fe7b594
Asuman Ssendegeya <asumanwebdev@gmail.com>
2024-02-29 17:23:14 +0300

    Update Cart.tsx

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -16,7 +16,7 @@ const Cart = () => {
       : [];
     setCartItems(myCartItems);
     //eslint-disable-next-line
-  }, []);
+  }, [quantity]);
 
   if (cartItems.length === 0) {
     // set totalQuantity to 0 in redux store when cart remains empty after removing the last item
